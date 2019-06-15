877cf6e2df46ca1ff9867fe015dd91c2e0b571ec
Mauricio <cmbarrososorio@gmail.com>
2019-06-14 19:30:37 -0500

    Fourth commit

--- solution.rb
+++ solution.rb
@@ -3,9 +3,14 @@ require 'sinatra'
 
 get '/' do
   unless params[:nombre]
-    "Hola desconocido!"
+    "<h1>Hola desconocido!</h1>"
   else
-    "Hola #{params[:nombre]}!"
+    "<h1>Hola #{params[:nombre]}!</h1>"
   end
+  #erb :index
+end
+
+get '/makers/:nombre' do |nombre|
+    "<h1>Hola #{nombre.to_s.capitalize}!</h1>"
   #erb :index
 end
